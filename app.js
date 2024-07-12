2985c69fd01ef351612ffd1949489fbfddd74579
basit1215 <haizabdulbasit225@gmail.com>
2024-07-12 23:47:05 +0500

    updated

---
+++ app.js
@@ -0,0 +1,55 @@
+
+
+// function curentdateTime() {
+//     var date = new Date();
+//     document.write(date);
+// }
+
+// curentdateTime();
+
+// function printName() {
+//     var firstName = prompt("Enter your first name");
+//     var lastName = prompt("Enter your last name");
+//     var fullName = firstName + " " + lastName;
+//     document.write(fullName);
+// }
+
+// printName();
+
+// function sum() {
+//     var firstNum = +prompt("Enter first number to add");
+//     var secondNum = +prompt("Enter second number to add");
+//     var totalNum = firstNum + secondNum;
+//     document.write(totalNum);
+
+// }
+
+// sum();
+
+// function calculator(num1, num2, operator) {
+//     var num1 = +prompt("For calculator, Enter first number");
+//     var num2 = +prompt("For calculator, Enter second number");
+//     var operator = prompt("Enter Operator");
+//     var result;
+//     if (operator === "+") {
+//         result = num1 + num2;
+//     }
+//     else if (operator === "-") {
+//         result = num1 - num2;
+//     }
+//     else if (operator === "/") {
+//         result = num1 / num2;
+//     }
+//     else if (operator === "*") {
+//         result = num1 * num2;
+//     }
+//     else if (operator === "%") {
+//         result = num1 % num2;
+//     }
+//     else {
+//         result = "Invalid Operator"
+//     }
+//     document.write("Result: " + result)
+// }
+
+// calculator();
